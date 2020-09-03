838d863a953c75ba3f3dfb35ee8503f7c170cf71
Colin Brayton <gringolalia@gmail.com>
2020-09-03 13:55:34 -0300

    deploy

---
+++ precache-manifest.94ce1e0a395c46c97a99c3ff36426641.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3abaa13c22ef8afbed6a",
+    "url": "/css/app.4da42f76.css"
+  },
+  {
+    "revision": "fb2e8759e5c7689c5e12",
+    "url": "/css/chunk-vendors.0bab466b.css"
+  },
+  {
+    "revision": "20a42ef12aac41cac1c8969c5758948e",
+    "url": "/fonts/inkline.20a42ef1.woff"
+  },
+  {
+    "revision": "ebe25a09f8c6844b928dae1ffa65ac79",
+    "url": "/fonts/inkline.ebe25a09.ttf"
+  },
+  {
+    "revision": "ff99d3fb75eedf667974b02c42e391d2",
+    "url": "/fonts/inkline.ff99d3fb.eot"
+  },
+  {
+    "revision": "50b34689795acb1a653ab1825bf8690c",
+    "url": "/img/infernalmachine.50b34689.jpg"
+  },
+  {
+    "revision": "4e95d343153b8ce342e83f48e7202fe9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "172365ea0df745e5bcee",
+    "url": "/js/about.be24927a.js"
+  },
+  {
+    "revision": "3abaa13c22ef8afbed6a",
+    "url": "/js/app.2fd1e3ec.js"
+  },
+  {
+    "revision": "fb2e8759e5c7689c5e12",
+    "url": "/js/chunk-vendors.cc0f0b94.js"
+  },
+  {
+    "revision": "21d4ff73cd870503f9029c41ff40da72",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
